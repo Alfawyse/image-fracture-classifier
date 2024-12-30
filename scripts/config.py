a99e071707fc1ee31fd12f17bca10e69b4777a46
Edward <eafanadoracevedo@gmail.com>
2024-12-30 16:08:55 -0500

    Initial setup: added project structure, scripts for data loading, model, training, evaluation, visualization, and configuration. Excluded 'data/' in .gitignore.

---
+++ scripts/config.py
@@ -0,0 +1,4 @@
+# Configuración general
+DATA_DIR = '../data/'  # Directorio donde están las imágenes
+IMAGE_SIZE = (224, 224)      # Tamaño de las imágenes
+BATCH_SIZE = 32              # Tamaño del batch
